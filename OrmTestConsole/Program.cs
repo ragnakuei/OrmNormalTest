f4cf3a0999bfa6c9a81263dcdf62bb301d1a8b0d
Kuei Peng <ragnakuei@livemail.tw>
2018-06-24 13:38:54 +0800

    add console

---
+++ OrmTestConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Running;
+using OrmTest.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmTestConsole
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var _dapperDAL = new DapperDAL();
+            //Console.WriteLine(_dapperDAL.Get().Count);
+
+            //var _efDAL = new EfDAL();
+            //Console.WriteLine(_efDAL.Get().Count);
+
+            BenchmarkRunner.Run<Benchmark>();
+            Console.ReadLine();
+        }
+    }
+
+    [MemoryDiagnoser]
+    [InliningDiagnoser]
+    [TailCallDiagnoser]
+    public class Benchmark
+    {
+        private readonly TestClass _benchClass = new TestClass();
+
+        [Benchmark]
+        public void Dapper() => _benchClass.Dapper();
+
+        [Benchmark]
+        public void EF() => _benchClass.EF();
+
+    }
+}
---
+++ OrmTestConsole/TestClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OrmTest.DAL;
+
+namespace OrmTestConsole
+{
+    internal class TestClass
+    {
+        private IDAL _dapperDAL;
+        private IDAL _efDAL;
+
+        public TestClass()
+        {
+            _dapperDAL = new DapperDAL();
+            _dapperDAL.Get();
+
+            _efDAL     = new EfDAL();
+            _efDAL.Get();
+        }
+
+        internal void Dapper()
+        {
+            _dapperDAL = new DapperDAL();
+            _dapperDAL.Get();
+        }
+
+        internal void EF()
+        {
+            _efDAL     = new EfDAL();
+            _efDAL.Get();
+        }
+    }
+}
